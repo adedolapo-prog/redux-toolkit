ff7300e8350756fc5cd93d1fa85954387978beba
adedolapo-prog <shayne.doly94@gmail.com>
2022-07-10 13:06:25 +0100

    done with counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,10 +5,18 @@ import {
   reset,
   incrementByAmount,
 } from "../features/counter/counterSlice"
+import { useState } from "react"
 
 const Counter = (props) => {
+  const [initialEntry, setInitialEntry] = useState(0)
+  const entry = Number(initialEntry) || 0
   const count = useSelector((state) => state.counter.count)
   const dispatch = useDispatch()
+
+  const resetAllHandler = (e) => {
+    setInitialEntry(0)
+    dispatch(reset())
+  }
 
   const incrementHandler = (e) => {
     dispatch(increment())
@@ -30,6 +38,10 @@ const Counter = (props) => {
     dispatch(incrementByAmount({ minus: 5 }))
   }
 
+  const addValuehandler = (e) => {
+    dispatch(incrementByAmount({ plus: entry }))
+  }
+
   return (
     <section>
       <p>{count}</p>
@@ -39,6 +51,16 @@ const Counter = (props) => {
         <button onClick={resetHandler}>Reset</button>
         <button onClick={decreaseHandler}>-5</button>
         <button onClick={decrementHandler}>-</button>
+      </div>
+
+      <input
+        type="text"
+        value={entry}
+        onChange={(e) => setInitialEntry(e.target.value)}
+      />
+      <div>
+        <button onClick={addValuehandler}>Add Value</button>
+        <button onClick={resetAllHandler}>Reset All</button>
       </div>
     </section>
   )
